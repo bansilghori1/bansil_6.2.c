10a66f63995c6faaa45f63e6ffa9b5c8cdea7a20
bansilghori1 <136827896+bansilghori1@users.noreply.github.com>
2023-06-29 21:53:59 +0630

    second commit

--- bansil33.c
+++ bansil33.c
@@ -5,19 +5,18 @@
 
 int main() {
 
-int i,a;
+int n,i,a=0,b=1,s;
 printf("enter the any number :  ");
-scanf("%d",&i);
-for(a=1;i<=a;){
-printf("%d",a);
+scanf("%d",&n);
+printf("\nFibonacci Series is:\n\n\n %d %d ",a,b); 
 
-}
+for(i=2;i<n;i++){
+    s=a+b;
+    printf("%d ",s);
+    a=b;
+    b=s;
 
-
-
-
-
-
+    }
 return 0;
 getch();    
 }
